4d297c9fe200bc019ee3c5be72fbe8fbedc79e6f
Loy Maxwell <freq75hz@gmail.com>
2020-07-05 16:56:47 +0900

    ABC 172追加

--- main.cpp
+++ main.cpp
@@ -11,13 +11,15 @@ namespace {
     constexpr auto MOD = 1000000007LL;
     constexpr auto PI = 3.1415926535897932384626433832795;
     #define FOR(i, j, k, step) for (int i=j; i<k; i+=step)
-    #define RFORS(i, j, k, step) for (int i=j; i>=k; i-=step)
+    #define RFOR(i, j, k, step) for (int i=j; i>=k; i-=step)
     #define REP(i, k) for(int i = (0); i < (k); i++ ) 
     #define RREP(i, k) for(int i = j; i >= (k); i-- )
     #define FOREACH(it, l) for(auto it=l.begin(); it != l.end(); it++)
     #define ALL(cont) cont.begin(), cont.end()
     #define RALL(cont) cont.rbegin(), cont.rend()
     #define REV(cont)  reverse(ALL(cont))
+    #define SORT(cont) sort(ALL(cont))
+    #define RSORT(cont) sort(RALL(cont))
     template<typename C>ll SIZE(const C& c) { return static_cast<ll>(c.size()); }   
     template<typename T, typename U, typename Comp = less<>>
     static inline bool chmax(T& maxv, const U& value, Comp comp = {}) { if (comp(maxv, value)) { maxv = value; return true; } return false; }
@@ -32,7 +34,8 @@ struct fast_io {
 } fast_io;
 
 void solve() {
-
+    int a; cin >> a;
+    cout << a + (a * a) + (a * a * a) << "\n";
 }
 
 signed main()
@@ -40,4 +43,3 @@ signed main()
     solve();
     return 0;
 }
-
